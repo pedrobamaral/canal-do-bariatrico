8881a6964cedf9a6c217dc091b9a59306681e5ca
davichunlee <davichunlee@gmail.com>
2025-09-20 12:57:25 -0300

    feat BD inicial(1)

--- backend/prisma/migrations/20250920152413_mig1/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `email` on the `usuario` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `VarChar(100)`.
-
-*/
--- AlterTable
-ALTER TABLE `usuario` ADD COLUMN `dataCriacao` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    MODIFY `email` VARCHAR(100) NOT NULL;

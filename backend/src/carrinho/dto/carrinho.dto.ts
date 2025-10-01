0307adf959562cfacf3fd851e8e3a913ba2c0f21
Pedro Barreto Cavalcante do Amaral <127952757+pedrobamaral@users.noreply.github.com>
2025-10-01 19:46:30 -0300

    Revert "Feat/crud carrinho"

--- backend/src/carrinho/dto/carrinho.dto.ts
+++
@@ -1,5 +0,0 @@
-export type CarrinhoDto = {
-  id?: number;                 // gerado pelo Prisma
-  idUsuario?: number | null;   // FK única -> Usuario.id
-  produtoIds?: number[];       // IDs de produtos para conectar
-};

509c12e2a2159129402e668de453ce4570696cba
Pedro <pedrobcamaral@gmail.com>
2025-10-01 10:32:40 -0300

    feat(product): add full CRUD implementations for Product module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProdutoModule } from './produto/produto.module';
 
 @Module({
-  imports: [],
+  imports: [ProdutoModule],
   controllers: [AppController],
   providers: [AppService],
 })

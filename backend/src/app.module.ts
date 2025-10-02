a7e2658d70effbafccfe2d6e43064e16ef401451
Pedro Barreto Cavalcante do Amaral <127952757+pedrobamaral@users.noreply.github.com>
2025-10-02 09:58:21 -0300

    Revert "Revert 3 feat/crud carrinho"

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './database/prisma.module';
+import { UsuarioModule } from './users/user.module';
+import { ProdutoModule } from './produto/produto.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UsuarioModule, ProdutoModule],
   controllers: [AppController],
   providers: [AppService],
 })

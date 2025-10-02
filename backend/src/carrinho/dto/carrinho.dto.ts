08b841374cff7a92a4e3c6bb367ee6a321a357e5
Pedro <pedrobcamaral@gmail.com>
2025-10-02 09:53:09 -0300

    fix(carrinho): fix comments in dto

--- backend/src/carrinho/dto/carrinho.dto.ts
+++ backend/src/carrinho/dto/carrinho.dto.ts
@@ -1,5 +1,5 @@
 export type CarrinhoDto = {
   id?: number;                 // gerado pelo Prisma
   idUsuario?: number | null;   // FK única -> Usuario.id
-  produtoIds?: number[];       // IDs de produtos para conectar
+  produtoIds?: number[];       // IDs de produtos para conectar com o carrinho
 };

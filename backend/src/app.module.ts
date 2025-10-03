19f071c13adf91c47a02ed0e2b7324e80f31c363
Marcio Vieira <marciovds07@gmail.com>
2025-10-03 00:28:59 -0300

    feat - CRUD carrinho

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './database/prisma.module';
+import { PrismaModule } from '../prisma/prisma.module';
 import { UsuarioModule } from './users/user.module';
 import { ProdutoModule } from './produto/produto.module';
 import { PagamentoModule } from './pagamento/pagamento.module';
+import { CarrinhoModule } from './carrinho/carrinho.module';
 
 @Module({
-  imports: [PrismaModule, UsuarioModule, ProdutoModule, PagamentoModule],
+  imports: [PrismaModule, UsuarioModule, ProdutoModule, PagamentoModule, CarrinhoModule],
   controllers: [AppController],
   providers: [AppService],
 })

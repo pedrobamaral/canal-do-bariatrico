ce128f0026d07007559af3b8cbd2826cd59cd772
vinicius.A <vinicius.araruna18@gmail.com>
2026-01-26 17:04:49 -0300

    chore(config): configura regras de ESLint e ordenação de imports
    Adiciona plugin simple-import-sort, bloqueia console.log e any, e configura automação no VS Code.

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,26 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    // regras personalizadas para o codigo
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      //imports ordenados automaticamente
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+
+      // sem console.log (warn e error)
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+
+      // warning sobre o uso de 'any'
+      "@typescript-eslint/no-explicit-any": "warn",
+
+      // aviso de variaveis nao utilizadas
+      "@typescript-eslint/no-unused-vars": "warn"
+    },
+  },
   {
     ignores: [
       "node_modules/**",
